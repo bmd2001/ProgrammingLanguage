3e537f34f0e103cbbc1f9409482554a810f8a1c7
brexit2001 <davidebressani21@gmail.com>
2024-12-06 15:44:36 +0000

    Added main compiler script

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod tokenizer;
+mod parser;
+
+use std::fs;
+use std::env;
+use crate::tokenizer::Tokenizer;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    if args.len() < 2 {
+        eprintln!("Usage: BRS <file.brs>");
+        std::process::exit(1);
+    }
+    
+    let file_path = args[1].as_str();
+    println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+    let mut tokenizer = Tokenizer::new();
+    tokenizer.tokenize(&contents);
 }
