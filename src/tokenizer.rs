b220bf04c0f99e5b96ba523e338dc4e0306e7a24
brexit2001 <davidebressani21@gmail.com>
2024-12-06 16:11:18 +0000

    Tokenizer for numbers

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,5 +1,3 @@
-use std::cmp::PartialEq;
-
 pub struct Tokenizer {
     m_tokens : Vec<Token>,
     m_index: usize
@@ -13,7 +11,7 @@ impl Tokenizer {
     pub fn tokenize(&mut self, input: &str){
         dbg!(&input);
         let mut buf : Vec<char> = Vec::new();
-        let mut peek = self.peek(input);
+        let mut peek = self.peek(input, None);
         while peek.is_some() {
             let last_char = peek.unwrap();
             if last_char.is_alphanumeric() || buf.is_empty(){
@@ -25,25 +23,30 @@ impl Tokenizer {
                 buf.clear();
             };
             self.m_index += 1;
-            peek = self.peek(input);
+            peek = self.peek(input, None);
         }
         dbg!(self.m_tokens.len());
     }
 
-    fn peek(&mut self, input: &str) -> Option<char> {
-        if self.m_index >= input.len() {
+    fn peek(&mut self, input: &str, offset: Option<usize>) -> Option<char> {
+        let off = offset.unwrap_or(0);
+        if self.m_index + off >= input.len() {
             return None;
         }
-        let c = input.chars().nth(self.m_index).unwrap(); // Accessing nth char
+        let c = input.chars().nth(self.m_index+off).unwrap(); // Accessing nth char
         Some(c)
     }
-    
+
     fn check_buf(&mut self, buf : &Vec<char>) -> Option<Token> {
         let string_buf: String = buf.iter().collect();
         match string_buf.as_str() {
             "exit" => Some(Token::Exit {span : (0, self.m_index - 3)}),
             "(" => Some(Token::OpenParen),
             ")" => Some(Token::CloseParen),
+            // Check if the buffer contains only digits and the next character is not a digit
+            _ if string_buf.chars().all(|c| c.is_digit(10)) && !self.peek(string_buf.as_str(), Some(1)).unwrap_or('a').is_digit(10) => {
+                Some(Token::Number {value : string_buf.clone(), span: (0, self.m_index - string_buf.len()-1) })
+            },
             _ => None
         }
     }
