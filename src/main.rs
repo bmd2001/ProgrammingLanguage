ad84962188003a067a6dc74c56ac9c335c4beb4f
brexit2001 <davidebressani21@gmail.com>
2024-12-07 00:02:35 +0000

    Changed out files location

--- src/main.rs
+++ src/main.rs
@@ -18,18 +18,19 @@ fn main() {
     }
     
     let file_path = args[1].as_str();
-    let mut out_file : &String = &file_path.replace(".brs", ".asm");
+    let mut out_dir : &String = &String::from("./");
 
-    if args.contains(&"-o".to_string()){
-        let out_id = args.iter().position(|s| s == "-o").unwrap()+1;
-        if let Some(filename) = args.get(out_id) {
-            if filename.contains(".asm"){
-                out_file = filename;
-            }
+    if args.contains(&"--outdir".to_string()){
+        let out_id = args.iter().position(|s| s == "--outdir").unwrap()+1;
+        if let Some(dir) = args.get(out_id) {
+            out_dir = dir;
         }
     }
+    let out_asm_file: &String = &(out_dir.to_owned() + &*file_path.replace(".brs", ".asm"));
+    let out_o_file : &String = &(out_dir.to_owned() + &*file_path.replace(".brs", ".o"));
     println!("In file {file_path}");
-    println!("Out file {out_file}");
+    println!("Out file {out_asm_file}");
+    println!("Out file {out_o_file}");
 
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
@@ -47,15 +48,15 @@ fn main() {
         assembly = generator.get_out_assembly();
     }
     println!("Assembly:\n{assembly}");
-    dbg!(out_file);
-    fs::write(out_file, assembly).expect("Unable to write file");
+    dbg!(out_dir);
+    fs::write(out_asm_file, assembly).expect("Unable to write file");
     use std::process::Command;
 
     // Run nasm
     let nasm_command = Command::new("nasm")
         .arg("-f")
         .arg("macho64")
-        .arg(out_file)
+        .arg(out_asm_file)
         .output()
         .expect("Failed to execute nasm");
 
@@ -71,8 +72,8 @@ fn main() {
         .arg("-macos_version_min")
         .arg("10.9.0")
         .arg("-o")
-        .arg("out")
-        .arg(out_file.replace(".asm", ".o"))
+        .arg(out_dir.to_owned() + "out")
+        .arg(out_o_file)
         .arg("-e")
         .arg("_start")
         .arg("-static")
