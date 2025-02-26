7eebb186be2bee0984b0bffde44b969e5df0e630
brexit2001 <davidebressani21@gmail.com>
2025-02-26 23:20:26 +0100

    Added ReversePolishNotation Unit Tests

--- src/compiler/parser/expression_factory/reverse_polish_notation.rs
+++ src/compiler/parser/expression_factory/reverse_polish_notation.rs
@@ -31,6 +31,7 @@ impl<'a> ReversePolishNotation<'a>{
                 }
                 _ => {
                     self.log_error(ParserErrorType::ErrUnexpectedToken, &token);
+                    return None;
                 },
             }
             self.m_line_stream.advance_skip_tokens(1, true, |token| matches!(token, Some(Token::WhiteSpace {..})));
@@ -64,14 +65,17 @@ impl<'a> ReversePolishNotation<'a>{
             _ => {
                 while let Some(lhs_op) = self.m_stack.pop() {
                     let lhs_is_open_bracket = matches!(lhs_op, Operator::OpenBracket {..});
-                    let lhs_geq_precedence = lhs_op.precedence() >= rhs_op.precedence();
+                    let lhs_leq_precedence = lhs_op.precedence() <= rhs_op.precedence();
                     let not_eq_precedence = lhs_op.precedence() != rhs_op.precedence();
                     let rhs_right_associative = rhs_op.associativity().eq("Right");
-                    if lhs_is_open_bracket || (lhs_geq_precedence && (not_eq_precedence || rhs_right_associative)){
+                    if lhs_is_open_bracket || 
+                        (lhs_leq_precedence && 
+                            (not_eq_precedence || rhs_right_associative)){
                         self.m_stack.push(lhs_op);
                         break;
+                    } else {
+                        self.m_polish.push(Token::Operator(lhs_op));
                     }
-                    self.m_polish.push(Token::Operator(lhs_op));
                 }
                 self.m_stack.push(rhs_op);
             }
@@ -82,5 +86,151 @@ impl<'a> ReversePolishNotation<'a>{
     fn log_error(&self, error: ParserErrorType, token: &Token){
         let mut logger = self.m_logger.lock().unwrap();
         logger.log_error(error, token);
+    }
+}
+
+
+
+#[cfg(test)]
+mod test_reverse_polish_notation{
+    use crate::compiler::logger::Logger;
+    use crate::compiler::span::Span;
+    use super::*;
+    
+    #[test]
+    fn test_translation(){
+        let dummy_span = Span::new(0, 0, 0);
+        let expr = vec![
+            Token::Number { value: "1".to_string(), span: dummy_span },
+            Token::Operator(Operator::Plus {span:dummy_span}),
+            Token::ID { name: "x".to_string(), span: dummy_span },
+            Token::Operator(Operator::Plus {span:dummy_span}),
+            Token::Number { value: "1".to_string(), span: dummy_span },
+        ];
+        let exp_notation = vec![
+            Token::Number { value: "1".to_string(), span: dummy_span },
+            Token::ID { name: "x".to_string(), span: dummy_span },
+            Token::Operator(Operator::Plus {span:dummy_span}),
+            Token::Number { value: "1".to_string(), span: dummy_span },
+            Token::Operator(Operator::Plus {span:dummy_span})
+        ];
+        let logger = Arc::new(Mutex::new(ParserLogger::new("".to_string(), "".to_string())));
+        let mut token_stream = TokenStream::new(expr.clone(), logger.clone());
+        let polish = ReversePolishNotation::new(&mut token_stream, logger).create();
+        assert!(polish.is_some());
+        assert_eq!(polish.unwrap(), exp_notation);
+    }
+    
+    #[test]
+    fn test_bracketed_expr(){
+        let dummy_span = Span::new(0, 0, 0);
+        let expr = vec![
+            Token::Operator(Operator::OpenBracket {span: dummy_span}),
+            Token::Number { value: "1".to_string(), span: dummy_span },
+            Token::Operator(Operator::Plus {span:dummy_span}),
+            Token::ID { name: "x".to_string(), span: dummy_span },
+            Token::Operator(Operator::ClosedBracket {span:dummy_span}),
+            Token::Operator(Operator::Multiplication {span:dummy_span}),
+            Token::Number { value: "1".to_string(), span: dummy_span }
+        ];
+        let exp_notation = vec![
+            Token::Number { value: "1".to_string(), span: dummy_span },
+            Token::ID { name: "x".to_string(), span: dummy_span },
+            Token::Operator(Operator::Plus {span:dummy_span}),
+            Token::Number { value: "1".to_string(), span: dummy_span },
+            Token::Operator(Operator::Multiplication {span:dummy_span})
+        ];
+        let logger = Arc::new(Mutex::new(ParserLogger::new("".to_string(), "".to_string())));
+        let mut token_stream = TokenStream::new(expr.clone(), logger.clone());
+        let polish = ReversePolishNotation::new(&mut token_stream, logger).create();
+        assert!(polish.is_some());
+        assert_eq!(polish.unwrap(), exp_notation);
+    }
+    
+    #[test]
+    fn test_precedence(){
+        let dummy_span = Span::new(0, 0, 0);
+        let expr = vec![
+            Token::Number { value: "1".to_string(), span: dummy_span },
+            Token::Operator(Operator::Plus {span:dummy_span}),
+            Token::ID { name: "x".to_string(), span: dummy_span },
+            Token::Operator(Operator::Multiplication {span:dummy_span}),
+            Token::Number { value: "1".to_string(), span: dummy_span }
+        ];
+        let exp_notation = vec![
+            Token::Number { value: "1".to_string(), span: dummy_span },
+            Token::ID { name: "x".to_string(), span: dummy_span },
+            Token::Number { value: "1".to_string(), span: dummy_span },
+            Token::Operator(Operator::Multiplication {span:dummy_span}),
+            Token::Operator(Operator::Plus {span:dummy_span})
+        ];
+        let logger = Arc::new(Mutex::new(ParserLogger::new("".to_string(), "".to_string())));
+        let mut token_stream = TokenStream::new(expr.clone(), logger.clone());
+        let polish = ReversePolishNotation::new(&mut token_stream, logger).create();
+        assert!(polish.is_some());
+        assert_eq!(polish.unwrap(), exp_notation);
+    }
+    
+    #[test]
+    fn test_associativity(){
+        let dummy_span = Span::new(0, 0, 0);
+        let expr = vec![
+            Token::Number { value: "1".to_string(), span: dummy_span },
+            Token::Operator(Operator::Exponent {span:dummy_span}),
+            Token::Number { value: "2".to_string(), span: dummy_span },
+            Token::Operator(Operator::Exponent {span:dummy_span}),
+            Token::Number { value: "3".to_string(), span: dummy_span }
+        ];
+        let exp_notation = vec![
+            Token::Number { value: "1".to_string(), span: dummy_span },
+            Token::Number { value: "2".to_string(), span: dummy_span },
+            Token::Number { value: "3".to_string(), span: dummy_span },
+            Token::Operator(Operator::Exponent {span:dummy_span}),
+            Token::Operator(Operator::Exponent {span:dummy_span})
+        ];
+        let logger = Arc::new(Mutex::new(ParserLogger::new("".to_string(), "".to_string())));
+        let mut token_stream = TokenStream::new(expr.clone(), logger.clone());
+        let polish = ReversePolishNotation::new(&mut token_stream, logger).create();
+        assert!(polish.is_some());
+        assert_eq!(polish.unwrap(), exp_notation);
+    }
+    
+    #[test]
+    fn test_wrong_expr(){
+        let dummy_span = Span::new(0, 0, 0);
+        let expr = vec![
+            Token::Operator(Operator::OpenBracket {span:dummy_span}),
+            Token::Number { value: "1".to_string(), span: dummy_span },
+            Token::Operator(Operator::Plus {span:dummy_span}),
+            Token::Number { value: "2".to_string(), span: dummy_span },
+        ];
+        let logger = Arc::new(Mutex::new(ParserLogger::new("".to_string(), "".to_string())));
+        let mut token_stream = TokenStream::new(expr.clone(), logger.clone());
+        let polish = ReversePolishNotation::new(&mut token_stream, logger).create();
+        assert!(polish.is_none());
+    }
+    
+    #[test]
+    fn test_unexpected_token(){
+        let dummy_span = Span::new(0, 0, 0);
+        let expr = vec![
+            Token::Err {span: dummy_span}
+        ];
+        let logger = Arc::new(Mutex::new(ParserLogger::new("".to_string(), "".to_string())));
+        let mut token_stream = TokenStream::new(expr.clone(), logger.clone());
+        let polish = ReversePolishNotation::new(&mut token_stream, logger).create();
+        assert!(polish.is_none());
+    }
+    
+    #[test]
+    fn test_mismatched_brackets(){
+        let dummy_span = Span::new(0, 0, 0);
+        let expr = vec![
+            Token::Operator(Operator::ClosedBracket {span:dummy_span})
+        ];
+        let logger = Arc::new(Mutex::new(ParserLogger::new("".to_string(), "".to_string())));
+        let mut token_stream = TokenStream::new(expr.clone(), logger.clone());
+        let polish = ReversePolishNotation::new(&mut token_stream, logger).create();
+        assert!(polish.is_none());
     }
 }
